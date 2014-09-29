7fe680cb17a20b9fcace6055f0e8b69e397be86a
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-29 17:53:40 +1000

    rebuild

--- js/src.js
+++ js/src.js
@@ -3,7 +3,6 @@ var http = require('http');
 var concat = require('concat-stream');
 var h = require('hyperscript');
 var cuid = require('cuid');
-var reTrailingSlash = /\/.*$/;
 
 function displaySample(target, sample, code) {
   var el = h('pre', h('code', { className: 'javascript' }, code));
@@ -35,7 +34,7 @@ function loadSample(evt) {
     return document.querySelector(evt.target.dataset.code).classList.toggle('active');
   }
 
-  req = http.get(location.origin + location.pathname.replace(reTrailingSlash, '') + '/examples/' + sample + '.js');
+  req = http.get('examples/' + sample + '.js');
   req.on('response', function(res) {
     res.pipe(concat(function(data) {
       evt.target.dataset.code = displaySample(evt.target, sample, data);
